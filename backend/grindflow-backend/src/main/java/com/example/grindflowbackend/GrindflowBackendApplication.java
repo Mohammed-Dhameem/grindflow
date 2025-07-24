67f80bb4222205526f4cabb67fc21597d506e3b8
Mohammed-Dhameem <nmaldhameem115@gmail.com>
2025-07-24 21:39:04 +0530

    Initial commit: Added frontend and backend project structure

---
+++ backend/grindflow-backend/src/main/java/com/example/grindflowbackend/GrindflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.grindflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrindflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrindflowBackendApplication.class, args);
+    }
+
+}
---
+++ backend/grindflow-backend/src/test/java/com/example/grindflowbackend/GrindflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grindflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrindflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
