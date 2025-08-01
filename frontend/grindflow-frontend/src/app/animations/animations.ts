d43822b22805fe1bab0227b9c1fb5d5c3e30c1fd
Mohammed-Dhameem <nmaldhameem115@gmail.com>
2025-08-01 06:31:09 +0530

    Moved the auth page to new auth component

--- frontend/grindflow-frontend/src/app/animations/animations.ts
+++ frontend/grindflow-frontend/src/app/animations/animations.ts
@@ -1,11 +1,34 @@
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, transition, style, animate, query, sequence} from '@angular/animations';
 
 export const fade = trigger('fade', [
-    transition(':enter', [
+  transition('* <=> *', [
+    // Optional: Animate leaving element first
+    query(':leave', [
+      style({ opacity: 1 }),
+      animate('300ms ease-out', style({ opacity: 0 }))
+    ], { optional: true }),
+
+    // Then animate entering element
+    query(':enter', [
+      style({ opacity: 0 }),
+      animate('400ms ease-in', style({ opacity: 1 }))
+    ], { optional: true })
+  ])
+]);
+
+export const fadeSwitch = trigger('fadeSwitch', [
+  transition(':enter', []), // required for structural animation support
+  transition('* <=> *', [
+    sequence([
+      query(':leave', [
+        style({ opacity: 1 }),
+        animate('300ms ease-out', style({ opacity: 0 }))
+      ], { optional: true }),
+
+      query(':enter', [
         style({ opacity: 0 }),
-        animate('400ms ease-out', style({ opacity: 1 }))
-    ]),
-    transition(':leave', [
-        animate('300ms ease-in', style({ opacity: 0 }))
+        animate('400ms ease-in', style({ opacity: 1 }))
+      ], { optional: true })
     ])
+  ])
 ]);
--- frontend/grindflow-frontend/src/app/app.routes.ts
+++ frontend/grindflow-frontend/src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AuthGuard } from './authentication/guard/auth.guard';
+import { AuthPageComponent } from './auth-page/auth-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/auth',
-    pathMatch: 'full'
+    component: LandingPageComponent
   },
   {
     path: 'auth',
-    component: LandingPageComponent
+    component: AuthPageComponent
   },
   {
     path: 'home',
